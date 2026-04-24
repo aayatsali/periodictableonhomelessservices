75bb790541ebdfb6ef76642f7c91fcd2f357cea9
Aayat <157442886+aayatsali@users.noreply.github.com>
2026-04-24 15:50:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the button and the content div by their IDs
-    const openButton = document.getElementById('openButton');
-    const contentDiv = document.getElementById('contentDiv');
-    const close_modals = document.querySelectorAll('.close-modal');
-    const overlay = document.getElementById('overlay');
+document.querySelector("buttonID").addEventListener("click", handleClick); 
 
-
-    // Add an event listener to the button for the 'click' event
-    openButton.addEventListener('click', () => {
-        // Toggle the 'visible' class on the content div
-        // If the class is present, it removes it (hiding the div)
-        // If the class is absent, it adds it (showing the div)
-        contentDiv.classList.toggle('visible');
-    });
-});
+function handleClick() {
+    alert("something here");
+}
