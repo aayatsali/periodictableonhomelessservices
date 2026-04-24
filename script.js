c907d34db1c0b51186e4d0612dc4b0d10ae222fb
Aayat <157442886+aayatsali@users.noreply.github.com>
2026-04-24 15:55:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Wait for the DOM to be fully loaded
+for (var i = 0; i<document.querySelectorAll(".buttonid"))
+
 document.querySelector("buttonID").addEventListener("click", handleClick); 
 
 function handleClick() {
